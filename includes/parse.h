ef0beeec8add1b3900ce5b96a5b779c19f146764
Jaehyun Lee <jaehylee@student.42gyeongsan.kr>
2025-07-19 08:39:39 +0900

    reform data structures, prepare to switch libft..

--- includes/parse.h
+++ includes/parse.h
@@ -6,32 +6,24 @@
 /*   By: sipyeon <sipyeon@student.42gyeongsan.kr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 20:17:12 by sipyeon           #+#    #+#             */
-/*   Updated: 2025/07/18 21:07:46 by jaehylee         ###   ########.fr       */
+/*   Updated: 2025/07/19 08:25:28 by jaehylee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSE_H
 # define PARSE_H
 
-# define SPHERE		1
-# define PLANE		2
-# define CYLINDER	3
-
 # include "utils.h"
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdbool.h>
 
-t_point	rt_parse_coordinate(char *param);
-t_vec	rt_parse_vector(char *param);
-t_color	rt_parse_color(char *value);
+t_point3	rt_parse_coordinate(char *param);
+t_vec3		rt_parse_vector(char *param);
+t_color		rt_parse_color(char *value);
 
-double	ft_strtod(char *str);
+double		ft_strtod(char *str);
 
-int		rt_valid_file_format(char *file);
-void	rt_file_validate_and_save_data(char *file, t_rt_info *info);
+int			rt_valid_file_format(char *file);
+void		rt_file_validate_and_save_data(char *file, t_rt_info *info);
 
-int		rt_init_object(t_rt_info *info, int type, char **param, t_color albedo);
+int			rt_init_object(t_rt_info *info, int type, char **param, t_color albedo);
 
 #endif
--- includes/structures.h
+++ includes/structures.h
@@ -6,75 +6,77 @@
 /*   By: sipyeon <sipyeon@student.42gyeongsan.kr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 13:47:01 by sipyeon           #+#    #+#             */
-/*   Updated: 2025/07/18 21:18:10 by jaehylee         ###   ########.fr       */
+/*   Updated: 2025/07/19 08:24:35 by jaehylee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCTURES_H
 # define STRUCTURES_H
-# define FALSE 0
-# define TRUE 1
-# define SP 0
-# define LIGHT_POINT 1
-# define EPSILON 1e-6
-# define LUMEN 3  // 이 값을 조절하여 장면의 밝기를 조절할 수 있다.
+# define LUMEN 3
 
-typedef struct s_vec	t_color;
-typedef struct s_vec	t_point;
-typedef int				t_bool;
-typedef int				t_object_type;
-
-typedef struct s_vec
+typedef enum e_obj_type
 {
-	double	x;
-	double	y;
-	double	z;
-}	t_vec;
+	SPHERE,
+	CYLINDER,
+	PLANE,
+	LIGHT
+}	t_obj_type;
 
 typedef struct s_ray
 {
-	t_point	orig;
-	t_vec	dir;
+	t_point3	orig;
+	t_vec3		dir;
 }	t_ray;
 
 typedef struct s_sphere
 {
-	t_point	center;
-	double	radius;
-	double	radius2;
+	t_point3	center;
+	double		radius;
 }	t_sphere;
 
 typedef struct s_cylinder
 {
-	t_point	center;
-	double	radius;
-	double	radius2;
+	t_point3	base;
+	double		radius;
+	t_vec3		height_norm;
 }	t_cylinder;
 
-typedef struct s_object
+typedef struct s_plane
 {
-	t_object_type	type;
-	void			*element;
-	t_color			albedo;
-	struct s_object	*next;
-}	t_object;
+	t_point3	base;
+	t_vec3		norm;
+}	t_plane;
 
 typedef struct s_light
 {
-	t_point	origin;
-	t_color	light_color;
-	double	bright_ratio;
+	t_point3	origin;
+	double		bright_ratio;
 }	t_light;
+
+typedef struct s_obj
+{
+	t_obj_type		type;
+	union u_mat
+	{
+		t_sphere	sp;
+		t_cylinder	cy;
+		t_plane		pl;
+		t_light		light;
+	}	mat;
+	t_color			color;
+	t_color			albedo;
+	struct s_obj	*next;
+}	t_obj;
 
 typedef struct s_camera
 {
-	t_point	orig;
-	double	viewport_h;
-	double	viewport_w;
-	t_vec	horizontal;
-	t_vec	vertical;
-	double	focal_len;
-	t_point	left_bottom;
+	t_point3	orig;
+	double		viewport_h;
+	double		viewport_w;
+	t_vec3		horiz;
+	t_vec3		vert;
+	double		focal_len;
+	t_point3	left_bottom;
 }	t_camera;
 
 typedef struct s_canvas
@@ -86,41 +88,40 @@ typedef struct s_canvas
 
 typedef struct s_hit_record
 {
-	t_point	p;
-	t_vec	normal;
-	double	tmin;
-	double	tmax;
-	double	t;
-	t_bool	front_face;
-	t_color	albedo;
+	t_point3	p;
+	t_vec3		norm;
+	double		tmin;
+	double		tmax;
+	double		t;
+	_Bool		front_face;
+	t_color		albedo;
 }	t_hit_record;
 
 typedef struct s_scene
 {
 	t_canvas		canvas;
 	t_camera		camera;
-	t_object		*object;
-	t_object		*light;
+	t_obj			*objs;
 	t_color			ambient;
 	t_ray			ray;
 	t_hit_record	rec;
 }	t_scene;
 
-typedef struct s_data
+typedef struct s_image
 {
 	void	*ptr;
 	char	*addr;
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
-}	t_data;
+}	t_image;
 
-typedef struct s_mrt
+typedef struct s_rt
 {
 	void	*mlx;
 	void	*win;
-	t_data	img;
+	t_image	img;
 	t_scene	scene;
-}	t_mrt;
+}	t_rt;
 
 #endif
